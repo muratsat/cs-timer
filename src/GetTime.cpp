9d9c101ad28403ec987762fa53d6baf2913f9765
muratsat <muratsat@users.noreply.github.com>
2022-10-09 13:16:31 +0300

    code formatting

--- src/GetTime.cpp
+++ src/GetTime.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 #include <ctime>
 
 #include "../include/DataTime.hpp"
--- src/SetCurrentTime.cpp
+++ src/SetCurrentTime.cpp
@@ -5,5 +5,7 @@
 DataTime SetCurrentTime() {
   time_t time_now;
   time(&time_now);
-  return *localtime(&time_now);
+  DataTime time = *localtime(&time_now);
+  mktime(&time);
+  return time;
 }
