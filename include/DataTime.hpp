93f9d5262d57d2b46f4cdf766d3f9d23b9aa0573
muratsat <muratsat@users.noreply.github.com>
2022-10-09 12:50:05 +0300

    DataTime.hpp: added description comments

--- include/DataTime.hpp
+++ include/DataTime.hpp
@@ -2,8 +2,12 @@
 
 typedef struct tm DataTime;
 
+// Устанавливает текущее системное данного компьютера
 DataTime SetCurrentTime();
 
+// Преобразует данную в формат DataTimer
+// YY-MM-DD HH:MIN:SEC
 DataTime GetTime(char* time_string);
 
+// Печатает день недели (7-дневка, понедельник - 1 день)
 char PrintWeekDay(DataTime t);
